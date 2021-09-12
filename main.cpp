5d7d9b8bba7cf7fabee825a93c89dde38a6c532b
irgendeinDave <80263819+irgendeinDave@users.noreply.github.com>
2021-09-12 13:23:36 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,6 +6,22 @@
 using namespace std;
 using std::filesystem::recursive_directory_iterator;
 
+unsigned int randomBgId(std::vector<std::string> bgs);
+
+#ifdef _WIN32
+    //TODO: apply wallpaper on windows
+#else
+void applyWallpaper(std::vector<string> bgs)
+{
+    //apply random wallpaper
+    string cmd = "gsettings set org.gnome.desktop.background picture-uri file://" 
+        + bgs.at(randomBgId(bgs));
+
+    system(cmd.c_str());
+}
+#endif
+
+
 unsigned int randomBgId(std::vector<string> bgs)
 {
     srand(time(NULL));
@@ -13,20 +29,25 @@ unsigned int randomBgId(std::vector<string> bgs)
     return result;
 }
 
-int main(int argc, char** argv) {
-    string path = argv[1];
+
+int main(int argc, char** argv) 
+{
+    string path;
+    if (argc == 2)
+        path = argv[1];
+    else 
+    {
+        cout << "Please enter a valid path for your wallpapers: ";
+        cin >> path;
+    }
+
     
     //get all items in directory and all subdirectories
     std::vector<string> backgrounds;
     for (const auto & file : recursive_directory_iterator(path))
         backgrounds.push_back(file.path());
 
-
-    //apply random wallpaper
-    string cmd = "gsettings set org.gnome.desktop.background picture-uri file://" 
-        + backgrounds.at(randomBgId(backgrounds));
-
-    system(cmd.c_str());
+    applyWallpaper(backgrounds);
     
     return EXIT_SUCCESS;
 }
